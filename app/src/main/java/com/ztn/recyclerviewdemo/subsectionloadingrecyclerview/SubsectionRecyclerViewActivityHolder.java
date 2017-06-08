70f0489303a77443b5c84cd16a21267a3bcab85f
逝去的那些年 <1458369273@qq.com>
2017-06-08 14:48:25 +0800

    添加了分段加载的Recyclerview

---
+++ app/src/main/java/com/ztn/recyclerviewdemo/subsectionloadingrecyclerview/SubsectionRecyclerViewActivityHolder.java
@@ -0,0 +1,21 @@
+package com.ztn.recyclerviewdemo.subsectionloadingrecyclerview;
+
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.ztn.common.BaseViewFinder;
+import com.ztn.recyclerviewdemo.R;
+
+/**
+ * Created by ztn on 2017/5/3
+ *
+ * @see SubsectionRecyclerViewActivity
+ */
+
+public class SubsectionRecyclerViewActivityHolder extends BaseViewFinder {
+
+    protected SubsectionRecyclerViewActivityHolder(Activity activity) {
+        super(activity);
+    }
+}
