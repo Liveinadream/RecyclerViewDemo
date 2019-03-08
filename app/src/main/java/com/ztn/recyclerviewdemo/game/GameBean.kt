9148d4b0f71b30f4b145f6b761678211851fd323
张天宁 <zhangtianning@bjknrt.com>
2019-03-08 11:38:48 +0800

    修改注释说明

--- app/src/main/java/com/ztn/recyclerviewdemo/game/GameBean.kt
+++ app/src/main/java/com/ztn/recyclerviewdemo/game/GameBean.kt
@@ -4,5 +4,6 @@ package com.ztn.recyclerviewdemo.game
  * Created by 冒险者ztn on 2019/3/7.
  * 游戏各自 bean
  * 1：自己。2：敌人。3：是否碰撞
+ * 说实话，第三个参数没啥用，但是懒得改了
  */
 data class GameBean(var gz: Triple<Boolean, Boolean, Boolean>)
